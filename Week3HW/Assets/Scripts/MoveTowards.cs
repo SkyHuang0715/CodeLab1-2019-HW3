f1bcb3d4b4a319dc8cc02dce827de415a03a7741
SkyHuang0715 <she515996344@gmail.com>
2019-02-18 04:51:59 -0500

    Get SceneManager works

---
+++ Week3HW/Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveTowards : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Scene scene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //throw new System.NotImplementedException();
+        transform.position = new Vector3(6, 5, 0);
+        
+        SceneManager.LoadScene("scene2");
+    }
+}
